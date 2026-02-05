d83449bf497175a26fe57df8c55e501e032fbac2
Abdullah Solahudeen <solahudeen15@gmail.com>
2026-02-05 19:45:00 +0100

    Update widget layout to row on mobile

--- src/app/(dashboard)/page.tsx
+++ src/app/(dashboard)/page.tsx
@@ -14,7 +14,7 @@ export default function DashboardPage() {
           <SalesOverview />
         </div>
 
-        <div className="xl:w-[35%] flex flex-col gap-6">
+        <div className="xl:w-[35%] flex flex-row xl:flex-col gap-6">
           <div className="flex-1">
             <WidgetCard
               title="Listings Overview"
