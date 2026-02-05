e0f03a4a5e5e96cb216ceea7dc405985c292b1c8
Abdullah Solahudeen <solahudeen15@gmail.com>
2026-02-05 14:21:48 +0100

    chore: fix Tailwind class suggestions and consolidate config files

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,16 @@
-import type { NextConfig } from 'next';
+import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
     reactStrictMode: true,
 
     // Image optimization
     images: {
-        remotePatterns: [],
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'picsum.photos',
+            },
+        ],
         formats: ['image/avif', 'image/webp'],
         deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
         imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
@@ -29,9 +34,9 @@ const nextConfig: NextConfig = {
     webpack: (config) => {
         config.resolve.alias = {
             ...config.resolve.alias,
-        };
-        return config;
+        }
+        return config
     },
-};
+}
 
-export default nextConfig;
+export default nextConfig
